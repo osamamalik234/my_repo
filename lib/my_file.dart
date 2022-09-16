3dac76ec100a251f97dce260500e7ba458731f2a
osamamalik234 <osamamalik9171@gmail.com>
2022-09-16 09:06:28 -0700

    Sixth Commit

---
+++ lib/my_file.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyFIle extends StatelessWidget {
+  const MyFIle({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My bro'),
+        centerTitle: true,
+      ),
+    );
+  }
+}
